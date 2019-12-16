715f45f571d6d5beb0413af14328d5937e0e87b9
TDS-G35 <TDS-G35@LabDocSyC.es>
2019-12-16 19:28:05 +0100

    Remove not needed files

--- TODOs.m
+++
@@ -1,3 +0,0 @@
-% Pasarlo a tiempo real
-% Parametrizar las frecuencias de muestreo
-% Parametrizar las frecuencias de bajo, alto, banda
--- test.m
+++
@@ -1,9 +0,0 @@
-%
-% Test con segnal grabada
-%
-
-clear;
-
-[s,Fs] = audioread('piano.wav');
-medirSegnal(s,Fs,'piano.wav');
-soundsc(s,Fs);
